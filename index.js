1477a31d4fccf653c27a007ddf76206794c4e75d
Felipe Passos <felipekaires@hotmail.com>
2017-10-21 14:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(obj,key,value) {
   obj.key = value
   return obj
 }
+
+function deleteFromObjectByKey(obj,key) {
+  var obj1 = Object.assign({},obj)
+  
+}
